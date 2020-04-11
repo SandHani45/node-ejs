5d9c259b7cb8d5f9f1b8c1f42155ab8f8a8ec571
Shaik <sandhani.sk45@gmail.com>
2020-04-11 21:21:01 +0530

    contact us done

--- routes/userView.js
+++ routes/userView.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+const { omit } = require("lodash");
+var nodemailer = require('nodemailer');
 // Guruji Model
 let Guruji = require('../models/guruji');
 
@@ -45,6 +47,51 @@ router.get('/our-inspirations', function(req, res) {
 // join-us
 router.get('/join-us', function(req, res) {
   res.render('pages/join-us',{layout:'layoutUser'});
+});
+
+// thanks
+router.get('/thanks', function(res,req){
+  res.render('pages/thanks',{layout:'layoutUser'});
+})
+
+//Contact Us
+router.get('/contact-us', function(req, res) {
+  res.render('pages/contact-us',{layout:'layoutUser'});
+});
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+      user:  'alessandro.luettgen7@ethereal.email', // generated ethereal user
+      pass: 'Ec7uqqpU8TvJmS1QzV' // generated ethereal password
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+router.post('/contact-us', function(req, res) {
+  let contact = {};
+  contact.firstName = req.body.firstName;
+  contact.message = req.body.message;
+  contact.email = req.body.email;
+  contact.lastName = req.body.lastName
+  var mailOptions = {
+    from: 'sandhani.sk45@gmail.com',
+    to: req.body.email,
+    subject: `trying to contact ${req.body.firstName} `,
+    text: req.body.message
+  };
+  transport.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.render('pages/thanks',{layout:'layoutUser'});
+    }
+  });
 });
 
 module.exports = router;
